554084923f2753f6cd651c9e3b09a97b4c91c825
KurniawanSaputra222 <kurniawan22287@gmail.com>
2025-03-01 19:15:22 +0700

    Delete Pertemuan 1 directory

--- Pertemuan 1/Luas_Persegi_Panjang.py
+++
@@ -1,4 +0,0 @@
-p = int(input(f"Masukkan Panjang Persegi Panjang = "))
-l = int(input(f"Masukkan Lebar Persegi Panjang = "))
-z = p * l
-print(f"Hasil Luas Persegi Panjang dengan Panjang  = {p} x Luas = {l} = {z}")
--- Pertemuan 1/Luas_Segitiga.py
+++
@@ -1,5 +0,0 @@
-a = int(input(f"Masukkan alas segitiga = "))
-t = int(input(f"Masukkan tinggi segitiga = "))
-l = (1/2)*a*t
-
-print(f"Hasil luas segitiga  1/2 x {a} x {t}  = {l}")
